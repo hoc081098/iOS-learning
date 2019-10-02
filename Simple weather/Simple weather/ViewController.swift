2629643d6d8feb110a98d5c1c3cd5e9a87d9885e
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-02 22:35:08 +0700

    up

--- Simple weather/Simple weather/ViewController.swift
+++ Simple weather/Simple weather/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    private let locationManager = CLLocationManager.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.requestLocation()
+        }
     }
 
 
+}
+
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print(location)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Get location error: \(error.localizedDescription)")
+    }
 }
 
